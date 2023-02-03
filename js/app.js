1e8b210a31b0971d318d0b4496feabb64649bd39
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-03 16:10:17 +0100

    FEAT prompt

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 //creare lista email
-const emailUser =["mario.rossi@gmail.com","mario.rossi@outlook.it","mario.rossi@hotmail.it","mario.rossi@yahoo.it","mario.rossi@libero.it","mario.rossi@apple.com"]
-console.log(emailUser)
+const emailUserList =["mario.rossi@gmail.com","mario.rossi@outlook.it","mario.rossi@hotmail.it","mario.rossi@yahoo.it","mario.rossi@libero.it","mario.rossi@apple.com"]
+console.log(emailUserList)
 //chiedere email a utente
+let emailUser = prompt ('Inserire email')
+console.log(emailUser)
 //stampare email presente (ciclo + if)
