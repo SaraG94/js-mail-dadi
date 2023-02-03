3d637ce0dcb2f485f2ce4d6d94f4f603e2620ac0
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-03 16:04:05 +0100

    FEAT array email

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
-console.log('ciaoo')
+//creare lista email
+const emailUser =["mario.rossi@gmail.com","mario.rossi@outlook.it","mario.rossi@hotmail.it","mario.rossi@yahoo.it","mario.rossi@libero.it","mario.rossi@apple.com"]
+console.log(emailUser)
+//chiedere email a utente
+//stampare email presente (ciclo + if)
